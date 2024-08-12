b8b57f789b36cf6464f973a241fc7017ee2254c1
lilyyllyyllyly <lilylylith@gmail.com>
2024-08-12 00:12:57 -0300

    Initial commit :3

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <signal.h>
+
+#include "scaffold.h"
+#include "mason.h"
+
+int end = 0;
+
+void handle_interrupt(int signal) {
+	end = 1;
+}
+
+int main() {
+	signal(SIGINT, handle_interrupt);
+
+	scaffold_node* root = scaffold_initialize();
+
+	scaffold_node* drawer = mason_drawer_create(128, 128, "boop :3", 60);
+	scaffold_node_add_child(root, drawer);
+
+	scaffold_node* sprite = mason_sprite_create(drawer, "assets/orange-kitten.png");
+	scaffold_node_add_child(root, sprite);
+
+	scaffold_node* sprite2 = mason_sprite_create(drawer, "assets/black-kitten.png");
+	sprite2->local_pos = (scaffold_vector2){16, 0};
+	scaffold_node_add_child(root, sprite2);
+
+	while (!end) {
+		scaffold_process_cleanup(root, mason_drawer_frame_time());
+	}
+
+	scaffold_queue_destroy(root);
+}
+
