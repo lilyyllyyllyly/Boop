f92a93c93d28f444cedeef40f258d543fe3ea47b
lilyyllyyllyly <lilylylith@gmail.com>
2024-08-13 04:06:25 -0300

    Resize game based on sprite size & columns/rows

--- src/main.c
+++ src/main.c
@@ -3,6 +3,14 @@
 #include "scaffold.h"
 #include "mason.h"
 
+#define DEFAULT_W 128
+#define DEFAULT_H 128
+#define TITLE "boop :3"
+#define FPS 0 // unlimited
+
+#define HCELLS 6
+#define VCELLS 6
+
 int end = 0;
 
 void handle_interrupt(int signal) {
@@ -20,13 +28,18 @@ int main() {
 	scaffold_node* sprite = mason_sprite_create(drawer, "assets/orange-kitten.png");
 	scaffold_node_add_child(root, sprite);
 
-	scaffold_node* sprite2 = mason_sprite_create(drawer, "assets/black-kitten.png");
-	sprite2->local_pos = (scaffold_vector2){16, 0};
-	scaffold_node_add_child(root, sprite2);
+	scaffold_vector2 spr_size = ((mason_sprite_data*)(sprite->data))->size;
+	mason_drawer_set_game_size(drawer, (int)(spr_size.x)*HCELLS, (int)(spr_size.y)*VCELLS);
 
-	scaffold_node* rect = mason_rectangle_create(drawer, (scaffold_vector2){16, 16});
-	rect->local_pos = (scaffold_vector2){32, 0};
-	scaffold_node_add_child(root, rect);
+	for (int i = 0; i < VCELLS; ++i) {
+		for (int j = 0; j < HCELLS; ++j) {
+			if (i == 0 && j == 0) continue;
+
+			scaffold_node* sprite2 = mason_sprite_create(drawer, i % 2 == j % 2? "assets/orange-kitten.png" : "assets/black-kitten.png");
+			sprite2->local_pos = (scaffold_vector2){spr_size.x * i, spr_size.y * j};
+			scaffold_node_add_child(root, sprite2);
+		}
+	}
 
 	while (!end) {
 		scaffold_process_cleanup(root, mason_drawer_get_frame_time());
