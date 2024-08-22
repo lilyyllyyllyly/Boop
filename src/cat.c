408e6a21ff126855a779139275f01b238e039011
lilyyllyyllyly <lilylylith@gmail.com>
2024-08-21 22:28:05 -0300

    Fix crashes when cats being deleted twice (fr this time)

--- src/cat.c
+++ src/cat.c
@@ -24,7 +24,7 @@ void cat_move(scaffold_node* cat, int new_x, int new_y) {
 
 	// delete cat if it falls outside the board (they fall off the bed according to the rules)
 	if (!is_cell_valid(new_x, new_y)) {
-		cat->destroy(cat);
+		scaffold_queue_destroy(cat);
 		return;
 	}
 
