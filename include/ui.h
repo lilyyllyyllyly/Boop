43878bdc808d755cce93897069ae8db2d54cbb6c
lilyyllyyllyly <lilylylith@gmail.com>
2024-09-20 00:05:15 -0300

    Add hint label when choosing promotion line

--- include/ui.h
+++ include/ui.h
@@ -11,9 +11,14 @@
 #define TURN_P0_TEXT "Orange's Turn"
 #define TURN_P1_TEXT "Black's Turn"
 
+#define CHOOSING_FONT_SIZE 10
+#define CHOOSING_TEXT "Choose 3 cats to graduate..."
+
 extern int ui_type;
 
 typedef struct {
+	scaffold_node* drawer;
+
 	game_manager_data* game_manager;
 	int old_player;
 
@@ -21,6 +26,9 @@ typedef struct {
 	mason_sprite_data* turn_label_data;
 	int turn_p0_width;
 	int turn_p1_width;
+
+	scaffold_node* choosing_label;
+	int choosing_width;
 } ui_data;
 
 scaffold_node* ui_create(scaffold_node* drawer, game_manager_data* game_manager);
--- src/ui.c
+++ src/ui.c
@@ -14,12 +14,28 @@ static void destroy(scaffold_node* ui) {
 
 static void process(scaffold_node* ui, double delta) {
 	ui_data* data = (ui_data*)(ui->data);
-
 	int new_player = data->game_manager->curr_player->id;
+
+	if (data->game_manager->choosing_turn && !data->choosing_label) {
+		// choosing turn started, create hint label
+		scaffold_node* choosing_label = mason_label_create(data->drawer, TEXT_DRAW_ORDER, CHOOSING_TEXT, CHOOSING_FONT_SIZE);
+
+		choosing_label->local_pos = (scaffold_vector2){
+			.x = (GAME_W - data->choosing_width)/2.f,
+			.y = GAME_H - CHOOSING_FONT_SIZE,
+		};
+
+		scaffold_node_add_child(ui, choosing_label);
+		data->choosing_label = choosing_label;
+	} else if (!data->game_manager->choosing_turn && data->choosing_label) {
+		// choosing turn started, delete the label
+		scaffold_queue_destroy(data->choosing_label);
+		data->choosing_label = NULL;
+	}
 
 	if (new_player == data->old_player) goto end;
 
-	// turn changed
+	// turn changed, update turn text
 	data->turn_label_data->shape.text = new_player == 0? "Orange's Turn" : "Black's Turn";
 	data->turn_label->local_pos.x = (GAME_W - (new_player == 0? data->turn_p0_width : data->turn_p1_width))/2.f;
 
@@ -29,8 +45,11 @@ end:
 
 scaffold_node* ui_create(scaffold_node* drawer, game_manager_data* game_manager) {
 	ui_data* data = malloc(sizeof(ui_data));
-	data->old_player = 0;
+
+	data->drawer = drawer;
+
 	data->game_manager = game_manager;
+	data->old_player = 0;
 
 	scaffold_node* ui = scaffold_node_create(
 		&ui_type,
@@ -39,7 +58,7 @@ scaffold_node* ui_create(scaffold_node* drawer, game_manager_data* game_manager)
 		destroy
 	);
 
-	scaffold_node* turn_label = mason_label_create(drawer, TEXT_DRAW_ORDER, "Orange's Turn", TURN_FONT_SIZE);
+	scaffold_node* turn_label = mason_label_create(drawer, TEXT_DRAW_ORDER, TURN_P0_TEXT, TURN_FONT_SIZE);
 
 	data->turn_p0_width = mason_drawer_get_text_width(TURN_P0_TEXT, TURN_FONT_SIZE);
 	data->turn_p1_width = mason_drawer_get_text_width(TURN_P1_TEXT, TURN_FONT_SIZE);
@@ -48,6 +67,9 @@ scaffold_node* ui_create(scaffold_node* drawer, game_manager_data* game_manager)
 	scaffold_node_add_child(ui, turn_label);
 	data->turn_label = turn_label;
 	data->turn_label_data = (mason_sprite_data*)(turn_label->data);
+
+	data->choosing_label = NULL;
+	data->choosing_width = mason_drawer_get_text_width(CHOOSING_TEXT, CHOOSING_FONT_SIZE);
 
 	return ui;
 }
